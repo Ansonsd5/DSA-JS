86d6f12d1d1a513da94b60f220bb63b1176bddf6
Ansonsd5 <anson11sd@gmail.com>
2022-12-25 20:39:16 +0530

    Update optimizedSolution.js

--- optimizedSolution.js
+++ optimizedSolution.js
@@ -2,22 +2,21 @@
 
 // Output: [-4,4]
 
-// Explanation: From the example we can see that nums[1] + nums[6] == 0, 
+// Explanation: From the example we can see that nums[1] + nums[6] == 0,
 // which is the required target, so have to return [-4,4].
 
-function zeroSumOptimized(array){
-  let  left = 0;
-  let right = array.length-1;
+function zeroSumOptimized(array) {
+  let left = 0;
+  let right = array.length - 1;
 
-  while( left< right){
-    if(array[left]-array[right]== 0){
-        return [array[left],array[right]]
-    }
-    right--;
+  while (left < right) {
+    let sum = array[left] - array[right];
+    if (sum == 0) {
+      return [array[left], array[right]];
+    } else if (sum > 0) {
+      right--;
+    } else left++;
   }
-left ++;
-  
 }
-
-const result =  zeroSumOptimized([-5,-4,-3,-2,0,2,4,6,8]);
+const result = zeroSumOptimized([-10,-6,-5, -4, -3, -2, 0, 2, 4, 6, 8]);
 console.log(result);
