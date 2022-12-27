65258533a7b794c630cd88af5a5220aeed1212f2
Ansonsd5 <anson11sd@gmail.com>
2022-12-27 19:39:06 +0530

    Update uniqueNumber.js

--- uniqueNumber.js
+++ uniqueNumber.js
@@ -1,19 +1,19 @@
-// Count distinct element in an array 
+// Count distinct element in an array
 //Given array is sorted
 
-function uniqueNumbers(inputArray){
-    if(inputArray.length<1){
-       throw new Error ("There are no elements")
+function uniqueNumbers(inputArray) {
+  if (inputArray.length < 1) {
+    throw new Error("There are no elements");
+  }
+  let count = 0; //i points at first element of the array
+  for (let j = 1; j < inputArray.length; j++) {
+    // j points to second element of array
+    if (inputArray[j] != inputArray[j - 1]) {
+      //checking if the second element and first are distinct if they are then
+      count++; // increment i by one
     }
-  let  i=0;                                 //i points at first element of the array
-for ( let j=1; j<inputArray.length; j++){   // j points to second element of array
-    if(inputArray[j]!=inputArray[i]){       //checking if the second element and first are distinct if they are then 
-        i++;                                // increment i by one 
-        inputArray[i]= inputArray[j]        //replacing the same number with distinct number
-    }
-   
+  }
+  return count + 1; //since i started with zeroth index increment it by one
 }
-return i +1;                                //since i started with zeroth index increment it by one
-}
-const num = uniqueNumbers([2,3,5,5,5,6,6,6,8,9,9,9,10]);
+const num = uniqueNumbers([-2, 3, 5, 5, 6, 6, 6,7, 8, 9, 9, 9, 10]);
 console.log(num);
