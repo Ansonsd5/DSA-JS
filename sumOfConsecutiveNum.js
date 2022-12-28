55835fd29c766a5c00d598bdf36e2bcbd18735c7
Ansonsd5 <anson11sd@gmail.com>
2022-12-28 23:57:16 +0530

    Create sumOfConsecutiveNum.js

---
+++ sumOfConsecutiveNum.js
@@ -0,0 +1,22 @@
+// Largest Sum of n consecutive numbers 
+
+function largestSum(array, num){
+
+    let temp=0; max=0;
+    if(num > array.length){
+        throw new Error("Given number is too big")
+    }
+    else{
+        for( i=0; i< array.length -num +1; i++){
+            {
+                for(j=0; j<num; j++){
+                    
+                }
+            }
+        }
+    }
+
+}
+
+// const result = largestSum([1,3,2,5,7,3,8,4],3)
+largestSum([1,3,2,5,7,3,8,4],6)
